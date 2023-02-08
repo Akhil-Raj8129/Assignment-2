ce2c6bf73c0d5104241a1f5c3f88d1cc2ce6b9a2
Akhil-Raj8129 <akhilrajpkd96@gmail.com>
2023-02-08 19:38:31 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+.container{
+    border: 2px solid #00f;
+    display: flex;
+    height: 550px;
+
+    flex-wrap: wrap;
+    align-content:space-between; 
+    
+}
+
+
+.hello1{
+    background-color: #00f;
+}
+.hello2{
+    background-color: rgb(0, 255, 170);
+}
+.hello3{
+    background-color: rgb(242, 0, 255);
+}
+.hello4{
+    background-color: rgb(255, 0, 47);
+}
+.hello5{
+    background-color: rgb(251, 255, 0);
+}
+.hello6{
+    background-color: rgb(30, 255, 0);
+}
+.container div{
+    width: 250px;
+}
